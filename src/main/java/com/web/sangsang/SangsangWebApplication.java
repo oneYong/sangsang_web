d10f307b5563351239dc3ddf694a4b9e79446608
kimwy <iersans@gmail.com>
2017-01-02 15:19:50 +0900

    first commit

---
+++ src/main/java/com/web/sangsang/SangsangWebApplication.java
@@ -0,0 +1,12 @@
+package com.web.sangsang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SangsangWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SangsangWebApplication.class, args);
+	}
+}
