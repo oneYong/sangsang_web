fb210d27c9053039fe0736adfa02ae625ef76281
neutti <pig@neuttiui-Mac-mini.local>
2017-03-03 16:14:01 +0900

    Resource Controller error 처리

--- src/main/java/com/web/sangsang/cmm/api/ResourceController.java
+++ src/main/java/com/web/sangsang/cmm/api/ResourceController.java
@@ -5,16 +5,17 @@ import com.web.sangsang.SangsangWebApplication;
 import com.web.sangsang.cmm.entity.SsResource;
 import com.web.sangsang.cmm.entity.annotation.Table;
 import com.web.sangsang.mapper.CmmMapper;
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
@@ -42,12 +43,12 @@ public class ResourceController {
     private ApplicationContext context;
 
     @RequestMapping(value="/{targetName}/{targetId}", method = RequestMethod.GET)
-    public void toImage(HttpServletRequest request, HttpServletResponse response
-                        ,@PathVariable("targetName") String targetName
-                        ,@PathVariable("targetId") String targetId
-                        ,@RequestParam(value = "type",required = false) String type
-                        ,@RequestParam(value = "maxWidth",required = false) Integer maxWidth
-                        ,@RequestParam(value = "maxHeight",required = false) Integer maxHeight){
+    public ResponseEntity toImage(HttpServletRequest request, HttpServletResponse response
+                        , @PathVariable("targetName") String targetName
+                        , @PathVariable("targetId") String targetId
+                        , @RequestParam(value = "type",required = false) String type
+                        , @RequestParam(value = "maxWidth",required = false) Integer maxWidth
+                        , @RequestParam(value = "maxHeight",required = false) Integer maxHeight){
         if(type == null) type = "png";
         if(maxWidth == null) maxWidth = 500;
         if(maxHeight == null) maxHeight = 500;
@@ -57,12 +58,7 @@ public class ResourceController {
             Map<String,Object> resource = cmmMapper.selectTable(SsResource.class.getAnnotation(Table.class).name(), whereClause);
             String data;
             if(resource == null){
-                //Image Empty
-                Resource imageResource = context.getResource("classpath:templates/resource/empty_image.jpg");
-                BufferedImage image = ImageIO.read(imageResource.getInputStream());
-                OutputStream out = response.getOutputStream();
-                ImageIO.write(image, type, out);
-                out.close();
+                return new ResponseEntity(HttpStatus.NOT_FOUND);
             }else{
                 data = (String) resource.get("contents");
                 byte[] imageBytes = javax.xml.bind.DatatypeConverter.parseBase64Binary(data);
@@ -97,10 +93,11 @@ public class ResourceController {
                 ImageIO.write(resizedImage, type, out);
                 out.close();
             }
+            return new ResponseEntity(HttpStatus.OK);
         } catch (IOException e) {
-            e.printStackTrace();
+            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
         } catch (Exception e) {
-            e.printStackTrace();
+            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
